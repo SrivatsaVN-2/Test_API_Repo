5d612000f4b31e3a2445ff83cc44f841ace8369c
SrivatsaVN-2 <srivatsa.nalluraya@innowave.tech>
2026-03-13 14:16:34 +0530

    Update Interface.py

--- APIs/dtdl/Interface.py
+++ APIs/dtdl/Interface.py
@@ -1,1 +1,8 @@
+from APIs.dtdl.epg_api import EPG_API
 
+class Interface:
+  def __init__("""fields will be updated"""):
+    #fields will be updated
+
+  def epg_api(): #will be updated
+  def home_api(): #will be updated
