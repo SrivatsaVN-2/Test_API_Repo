af8cd225158e653282a61944eecfb70a32f7a4a5
SrivatsaVN-2 <srivatsa.nalluraya@innowave.tech>
2026-03-13 15:49:08 +0530

    Update Interface.py

--- APIs/dtdl/Interface.py
+++ APIs/dtdl/Interface.py
@@ -1,8 +1,34 @@
-from APIs.dtdl.epg_api import EPG_API
-
 class Interface:
-  def __init__("""fields will be updated"""):
+  def __init__(self,language, user_and_device_details, major_version, natco_config, STBConfig):
     #fields will be updated
+    self.language = language
+    self.user_and_device_details = user_and_device_details
+    self.major_version = major_version
+    self.natco_config = natco_config
+    self.STBConfig = STBConfig
+    self._channel_api_client = None
+    self._home_api_client = None
+    self._epg_api_client = None
+  @property
+  def channel_api(self):
+    if self._channel_api_client is None:
+       from APIs.dtdl.channel_api import ChannelApiClient
 
-  def epg_api(): #will be updated
-  def home_api(): #will be updated
+       self._channel_api_client = ChannelApiClient(config_manager, self.natco_config)
+     return self._channel_api_client
+  
+  @property
+  def home_api(self):
+    if self._home_api_client is None:
+      from APIs.dtdl.home_api import HomeApiClient
+
+      self._home_api_client = HomeApiClient(config_manager, self.natco_config)
+  return self._home_api_client
+
+  @property
+  def epg_api(self):
+    if self._epg_api_client is None:
+      from APIs.dtdl.epg_api import EpgApiClient
+      self._epg_api_client = EpgApiClient(config_manager, self.natco_config)
+    return self._epg_api_client
+  
