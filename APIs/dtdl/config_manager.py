cfb630cdce85410c0905be95df3b271363a85e6e
Srivatsa Nalluraya <srivatsa.nalluraya@innowave.tech>
2026-03-16 12:29:51 +0530

    changes

--- APIs/dtdl/config_manager.py
+++ APIs/dtdl/config_manager.py
@@ -2,15 +2,7 @@
 # Handles reading and providing configuration data from a JSON file
 
 import json
-import os
 
-from tests.Test_API_Repo.APIs.dtdl.Interface import Interface
-
-"""
-from tests.androidtv.pages.helper import get_language
-from tests.androidtv.pages.utility.stbconfig import STBConfig
-from tests.androidtv.pages.utility.utils import Utils
-"""
 
 LANGUAGE_MAPPING = {
     ("at", "AT"): "de",
@@ -31,61 +23,72 @@ LANGUAGE_MAPPING = {
 
 
 class Config_Manager:
-    def __init__(self, config_file):
-        self.interface = Interface()
-        self.language = Interface.language
-        # print(os.getcwd())
+
+    def __init__(self, config_file, interface):
+        """
+        Config manager should not create Interface.
+        It receives Interface as a dependency.
+        """
+
+        self.interface = interface
+        self.language = interface.language
+        self.STBConfig = interface.STBConfig
+
         with open(config_file, "r") as file:
             self.config = json.load(file)
-
 
     def get_endpoint(self, lang, endpoint_type):
         return self.config["endpoints"].get(lang, {}).get(endpoint_type, "")
 
     def get_header(self, lang, header_type, token=""):
-        header = self.config["headers"].get(lang, {}).get(header_type, {})
-        # if header_type in ["OTHER", "BFF_OTHER"]:
-        #     header['Authorization'] = f'Bearer {token}'
-        # return header
-        # Check if token needs to be added to the header
+        header = self.config["headers"].get(lang, {}).get(header_type, {}).copy()
+
         if header_type == "OTHER":
             header["Authorization"] = f"Bearer {token}"
-            # print("Authorisation Token", token)
+
         elif header_type == "BFF_OTHER":
             header["bff_token"] = token
 
-            # Remove x-adult-token from config headers as it should come from stb_data.json
             if "x-adult-token" in header:
                 del header["x-adult-token"]
 
         return header
 
     def get_param(self, lang, param_type):
+
         params = self.config["params"].get(lang, {}).get(param_type, {}).copy()
+
         if "app_language" in params:
 
-            language = get_language()
+            language = self.interface.language
             key = (language, lang)
+
             if key in LANGUAGE_MAPPING:
                 params["app_language"] = LANGUAGE_MAPPING[key]
+
         return params
 
     def get_data(self, lang, data_type, username="", password=""):
-        user_infor = []
+
         data = self.config["data"].get(lang, {}).get(data_type, {}).copy()
-        device_id = STBConfig.adb_device_id
-        user_info = Utils().get_device_and_user_info(device_id)
-        user_id, user_infor = user_info[3], user_info[4]
-        # print("User info", user_infor)
-        # print("User id", user_id)
-        pwd = user_infor.get("passcode")
-        data["bff_token"] = user_infor.get("bff_token")
-        # print("STBConfig.fdn_natco ", STBConfig.fdn_natco)
-        if STBConfig.fdn_natco in ["HU SDMC", "HU SEI", "MKT"]:
-            # print("data", data)
+
+        device_id = self.STBConfig.adb_device_id
+
+        utils = self.interface.utils
+        user_info = utils.get_device_and_user_info(device_id)
+
+        user_id = user_info[3]
+        user_details = user_info[4]
+
+        pwd = user_details.get("passcode")
+
+        data["bff_token"] = user_details.get("bff_token")
+
+        if self.STBConfig.fdn_natco in ["HU SDMC", "HU SEI", "MKT"]:
             return data
 
         if data_type == "LOGIN":
+
             if username:
                 data["telekomLogin"]["username"] = username
             else:
@@ -95,6 +98,5 @@ class Config_Manager:
                 data["telekomLogin"]["password"] = password
             else:
                 data["telekomLogin"]["password"] = pwd
-        # print("Data", data)
-        return data
 
+        return data
