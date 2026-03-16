fbc6cfd08f41fdf5672fe78e3ef82c280f17045e
Srivatsa Nalluraya <srivatsa.nalluraya@innowave.tech>
2026-03-16 12:22:08 +0530

    changes

--- APIs/dtdl/Interface.py
+++ APIs/dtdl/Interface.py
@@ -1,4 +1,4 @@
-from APIs.dtdl.config_manager import Config_Manager
+from tests.Test_API_Repo.APIs.dtdl.config_manager import Config_Manager
 class Interface:
   def __init__(self,language, user_and_device_details, major_version, natco_config, STBConfig):
     #fields will be updated
