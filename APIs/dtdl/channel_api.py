b4ee04e3351280985bb46df672b1bb87a2538c7b
SrivatsaVN-2 <srivatsa.nalluraya@innowave.tech>
2026-03-17 15:10:21 +0530

    Update channel_api.py

--- APIs/dtdl/channel_api.py
+++ APIs/dtdl/channel_api.py
@@ -24,7 +24,7 @@ class ChannelApiClient(BaseApiClient):
 
             # ✅ NATCO from interface
             
-            self.natco = (self.interface.natco_config.get("natco") or "").upper()
+            self.natco = (self.interface.get_natco() or "").upper()
 
             # ✅ CMS Integration
             self.cms_client = self._init_cms_client()
