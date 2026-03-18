dc604a918f40caa78abbb9e7fd3f350137998ec4
SrivatsaVN-2 <srivatsa.nalluraya@innowave.tech>
2026-03-18 12:55:23 +0530

    Update recording_api.py

--- APIs/dtdl/recording_api.py
+++ APIs/dtdl/recording_api.py
@@ -1,6 +1,6 @@
 from tests.Test_API_Repo.APIs.dtdl.base_api_client import BaseApiClient
 from tests.Test_API_Repo.Utilities.Queries import APIQuery
-from tests.androidtv.pages.utility.system_logger import Logger
+from tests.Test_API_Repo.Utilities.Loggers import Logger
 
 
 log = Logger().setup_logger("API.Recording")
